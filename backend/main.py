732d0eacb34842518774097c904433c22ab7cf98
Doug <27059467+doug-newton@users.noreply.github.com>
2025-10-05 21:20:48 +0200

    use env variables for port and debug mode in flask app

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+port = int(os.environ.get('ABIOTIC_PORT', 3000))
+debug_on = int(os.environ.get('ABIOTIC_DEBUG', 0)) == 1
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=debug_on)
