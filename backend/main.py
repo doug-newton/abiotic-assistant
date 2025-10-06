1067f1b352a84f531c81b0dcfa686610774e71d0
Doug <27059467+doug-newton@users.noreply.github.com>
2025-10-06 12:56:11 +0200

    get frontend/backend to communicate in dev and prod mode
    backend:
    - install flask_cors (requirements-dev.txt) in backend/Dockerfile for
      dev
    - import and allow CORS for all clients if ABIOTIC_DEV env variable is 1
    - add sample api/v route
    
    frontend:
    - add VITE_APP_URL to .env files for production (8080) and development
      (3001)
    - add sample code to get the api version from backend and display it on
      screen

--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,19 @@ app = Flask(__name__)
 
 port = int(os.environ.get('ABIOTIC_PORT', 3000))
 debug_on = int(os.environ.get('ABIOTIC_DEBUG', 0)) == 1
+dev_mode = int(os.environ.get('ABIOTIC_DEV', 0)) == 1
+
+if dev_mode:
+    from flask_cors import CORS
+    CORS(app)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/api/v")
+def get_api_version():
+    return jsonify({"version": "0.01"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port, debug=debug_on)
