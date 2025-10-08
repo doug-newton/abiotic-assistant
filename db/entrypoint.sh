f8d10d981735fc3efe5d0fcaa02b9ba1489fc9d3
Doug <27059467+doug-newton@users.noreply.github.com>
2025-10-08 17:57:28 +0200

    simplify api credential swap

--- db/entrypoint.sh
+++ db/entrypoint.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-# load secrets into the api user creation script
-
 api_user_script=/docker-entrypoint-initdb.d/001-create-api-user.js
 
 tmpfile="$api_user_script.tmp"
 
-sed 's/__PASSWORD__/\"__PASSWORD__\"/' $api_user_script \
-	| sed "s/__PASSWORD__/$(cat /run/secrets/mongo_api_passwd)/" \
-	| sed 's/__USERNAME__/\"__USERNAME__\"/' \
-	| sed "s/__USERNAME__/$(cat /run/secrets/mongo_api_user)/" > $tmpfile
+sed "s/__USERNAME__/\"$(cat /run/secrets/mongo_api_user)\"/" $api_user_script \
+	| sed "s/__PASSWORD__/\"$(cat /run/secrets/mongo_api_passwd)\"/" > $tmpfile
 
 mv $tmpfile $api_user_script
 
