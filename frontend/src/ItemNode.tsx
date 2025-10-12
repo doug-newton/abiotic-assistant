cc338c726305102bf6cd997c75bed4b44ae08aec
Doug <27059467+doug-newton@users.noreply.github.com>
2025-10-12 13:04:03 +0200

    fix adding of multiple nodes and calculate positions relative to current

--- frontend/src/ItemNode.tsx
+++ frontend/src/ItemNode.tsx
@@ -1,5 +1,5 @@
 import type { Edge, Node, NodeProps } from '@xyflow/react'
-import { Handle, Position, useReactFlow } from '@xyflow/react'
+import { Handle, Position, useReactFlow, useInternalNode } from '@xyflow/react'
 import classes from './ItemNode.module.css'
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -24,44 +24,64 @@ async function getTransforms(item: string): Promise<ItemTransform[]> {
 
 export default function ItemNode({ id, data }: NodeProps<Node<ItemData>>) {
     const { item, quantity } = data;
-
+    const internalNode = useInternalNode(id);
     const { getNodes, addNodes, getEdges, addEdges } = useReactFlow();
 
     async function onClick() {
-        const itemTransforms : ItemTransform [] = await getTransforms(item);
+        const itemTransforms: ItemTransform[] = await getTransforms(item);
+
+        const currentX = internalNode ? internalNode.position.x : 0;
+        const currentY = internalNode ? internalNode.position.y : 0;
+        const blockSize = 200;
+
+        const itemX = currentX - blockSize;
+
+        const totalHeight = blockSize * (itemTransforms.length);
+        let blockTop = currentY - totalHeight / 2;
+
+        const newNodes: Node[] = [];
+        const newEdges: Edge[] = [];
+        let newNodeID = getNodes().length + 1;
+        let newEdgeID = getEdges().length + 1;
 
         for (let itemTransform of itemTransforms) {
-            const transformLabel: string = itemTransform.transform;
+            const itemStep = blockSize / (itemTransform.input.length + 1);
+            let itemY = blockTop + itemStep;
+
             for (let itemData of itemTransform.input) {
-                addSourceItem(itemData, transformLabel);
+                console.log(`${itemX}, ${itemY}`)
+                const node: Node<ItemData> = {
+                    type: 'itemNode',
+                    id: `${newNodeID}`,
+                    position: {
+                        x: itemX,
+                        y: itemY 
+                    },
+                    data: itemData
+                };
+
+                const edge: Edge = {
+                    id: `${newEdgeID}`,
+                    label: itemTransform.transform,
+                    source: `${newNodeID}`,
+                    target: id,
+                    animated: true
+                };
+
+                newNodes.push(node);
+                newEdges.push(edge);
+
+                newNodeID++;
+                newEdgeID++;
+
+                itemY += itemStep;
             }
+
+            blockTop += blockSize;
         }
-    }
 
-    function addSourceItem(itemData: ItemData, transformLabel: string) {
-        const newNodeID: string = `${getNodes().length + 1}`;
-        const newEdgeID: string = `${getEdges().length + 1}`;
-
-        const node: Node<ItemData> = {
-            type: 'itemNode',
-            id: newNodeID,
-            position: {
-                x: 0,
-                y: 0
-            },
-            data: itemData
-        };
-
-        const edge: Edge = {
-            id: newEdgeID,
-            label: transformLabel,
-            source: newNodeID,
-            target: id,
-            animated: true
-        };
-
-        addNodes(node);
-        addEdges(edge);
+        addNodes(newNodes);
+        addEdges(newEdges);
     }
 
     return (
