94f01990ddedd0042ac6817b707eab963bffa0c3
Doug <27059467+doug-newton@users.noreply.github.com>
2025-10-12 16:48:40 +0200

    handle request failures more gracefully

--- frontend/src/Api.ts
+++ frontend/src/Api.ts
@@ -5,6 +5,17 @@ const API_URL = import.meta.env.VITE_API_URL;
 export async function getTransforms(item: string): Promise<ItemTransform[]> {
     const url = `${API_URL}/transforms/${item}`;
     const response = await fetch(url);
-    const transforms: ItemTransform[] = await response.json();
-    return transforms;
+    
+    try {
+        if (!response.ok) {
+            throw new Error(`Server error ${response.status}`);
+        }
+
+        const transforms: ItemTransform[] = await response.json();
+        return transforms;
+    }
+    catch (error) {
+        console.error(`Couldn't fetch data: ${error}`);
+        return [];
+    }
 }
